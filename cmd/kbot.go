c9a7af3abd25c5769a3b11619eadb3847610f83f
Valerii Zapara <65090419+unweijoozd@users.noreply.github.com>
2024-04-02 18:26:16 +0000

    update to v1.0.2

--- cmd/kbot.go
+++ cmd/kbot.go
@@ -21,6 +21,7 @@ var (
 // kbotCmd represents the kbot command
 var kbotCmd = &cobra.Command{
 	Use:   "kbot",
+	Aliases: []string{"start"},
 	Short: "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
@@ -46,7 +47,14 @@ to quickly create a Cobra application.`,
 		kbot.Handle(telebot.OnText, func(m telebot.Context) error {
 
 			log.Print(m.Message().Payload, m.Text())
+			payload := m.Message().Payload
 
+			switch payload {
+			case "hello":
+				err = m.Send(fmt.Sprintf("Hello I'm Kbot %s!", appVersion))
+
+			}
+				
 			return err
 		})
 
